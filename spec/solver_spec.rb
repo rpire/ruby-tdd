d0b4b5f227fd36d9b3c5d39251e6e813c1ebff8f
rpire <rubenpire7@gmail.com>
2021-11-18 04:55:32 -0400

    Update the before each block for the factorials test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,14 +3,14 @@ require 'rspec'
 
 RSpec.describe Solver do
     before :each do
-      @factorial = Solver.factorial(4)
+      @solver = Solver.new
     end
 
     it 'Factorial input is an integer' do
-      @factorial.should be_an(Integer)
+      @solver.factorial(3).should be_an(Integer)
     end
 
     it 'Factorial works' do
-      @factorial.should eql 24
+      @solver.factorial(4).should eql 24
     end
 end
